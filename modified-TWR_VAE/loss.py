274b54d672a8b1768bf9ce12a94f5c79e749081d
Mincheol CHO <code1478@naver.com>
2022-11-18 19:22:16 +0900

    Update loss.py

--- modified-TWR_VAE/loss.py
+++ modified-TWR_VAE/loss.py
@@ -26,13 +26,13 @@ class Gamma_Family():
     def gamma_divergence(self, df):
         '''
         Generalized gamma divergence.
-        paramter : df > 2 (This is because the variance of T dist : df/(df-2), v>0)
+        paramter : df > 2 (This is because the variance of T dist : df/(df-2), v>2)
         
         cf) Instead of gamma, we use df(degree of freedom) as a paramter. (gamma = -2 /(1+df))
         '''
         # Check the well-definedness
         if df <= 2:
-            raise Exception(f'the degree of freedom is not positive. Divergence is not well-defined.')
+            raise Exception(f'the degree of freedom is not larger than 2. Divergence is not well-defined.')
 
 
 
