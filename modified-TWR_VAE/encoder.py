a64af551d863381d9a51f822625ef03ffd016faa
Mincheol CHO <code1478@naver.com>
2022-12-06 17:28:34 +0900

    Update encoder.py

--- modified-TWR_VAE/encoder.py
+++ modified-TWR_VAE/encoder.py
@@ -51,7 +51,7 @@ class Encoder(nn.Module):
                 eps = torch.randn_like(std) # Normal dist
             else:
                 Tdist = torch.distributions.studentT.StudentT(self.df)
-                eps = Tdist.sample() # Student T dist
+                eps = Tdist.sample(sample_shape = torch.Size(prior_mu.shape)) # Student T dist
 
             if not self.training and mi:
                 return mu, mu + eps*std
